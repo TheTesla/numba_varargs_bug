44e99ce444d83edc1e75b8d86e86a138cc461a52
Stefan Helmert <stefan.helmert@t-online.de>
2024-11-10 15:55:01 +0100

    add expected result

--- expected.py
+++ expected.py
@@ -1,42 +1,38 @@
-from numba import njit
 
-@njit
 def f0(*args):
     for i, a in enumerate(args):
         print("  args[", i, "] =", a)
 
-@njit
 def f1(*args, x):
     print("  x =", x)
     for i, a in enumerate(args):
         print("  args[", i, "] =", a)
 
-@njit
 def f2(*args, x=8.8):
     print("  x =", x)
     for i, a in enumerate(args):
         print("  args[", i, "] =", a)
 
-@njit
 def f3(x, *args):
     print("  x =", x)
     for i, a in enumerate(args):
         print("  args[", i, "] =", a)
 
-@njit
 def f4(x=8.8, *args):
     print("  x =", x)
     for i, a in enumerate(args):
         print("  args[", i, "] =", a)
 
-@njit
 def f():
     print("")
-    print("actual results")
+    print("expected results")
     print("f0(1.1,2.2,3.3,4.4,5.5,6.6):")
     f0(1.1,2.2,3.3,4.4,5.5,6.6)
     print("f1(1.1,2.2,3.3,4.4,5.5,6.6):")
-    f1(1.1,2.2,3.3,4.4,5.5,6.6)
+    try:
+        f1(1.1,2.2,3.3,4.4,5.5,6.6)
+    except Exception as e:
+        print(e)
     print("f1(1.1,2.2,3.3,4.4,5.5,x=6.6):")
     f1(1.1,2.2,3.3,4.4,5.5,x=6.6)
     print("f2(1.1,2.2,3.3,4.4,5.5,6.6):")
@@ -45,9 +41,9 @@ def f():
     f2(1.1,2.2,3.3,4.4,5.5,x=6.6)
     print("f3(1.1,2.2,3.3,4.4,5.5,6.6):")
     f3(1.1,2.2,3.3,4.4,5.5,6.6)
-    #print("f3(1.1,2.2,3.3,4.4,5.5,x=6.6):")
+    print("f3(1.1,2.2,3.3,4.4,5.5,x=6.6):")
     #f3(1.1,2.2,3.3,4.4,5.5,x=6.6)
-    print("f4(1.1,2.2,3.3,4.4,5.5,6.6):")
+    #print("f4(1.1,2.2,3.3,4.4,5.5,6.6):")
     f4(1.1,2.2,3.3,4.4,5.5,6.6)
     #print("f4(1.1,2.2,3.3,4.4,5.5,x=6.6):")
     #f4(1.1,2.2,3.3,4.4,5.5,x=6.6)
